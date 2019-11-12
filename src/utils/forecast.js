96e774ff91c39d685d3fb503f3cd1533ad306a27
Jared Desvaux de Marigny <jared.ddm@gmail.com>
2019-11-13 01:47:30 +1300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/2af6ca100bbd9997943c93d959766631/' + latitude + ',' + longitude + '?units=si'
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!')
+        } else if (body.error) {
+            callback('Unable to find location!')
+        } else {
+            const summary = body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is a ${(body.currently.precipProbability * 100)}% chance of rain.`
+            callback(undefined, summary)
+        }
+    })
+}
+
+module.exports = forecast
